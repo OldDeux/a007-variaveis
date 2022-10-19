e8ff632ffaaf00d3db6a1b1fb9b988d308fb30a7
victor felipe <vitinhose46@gmail.com>
2022-10-18 22:45:10 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,27 @@
+/* //parte1
+const nome = prompt("qual o seu nome?")
+let idade = prompt("qual a sua idade?")
+
+
+
+console.log(typeof nome, typeof Number(idade) )
+// ao usar const com um texto a primeira variavel se torna uma string,  ao colocar let e um numero minha segunda variavel se torna um number//
+
+console.log("ola" ,nome, "voce tem" ,idade, "anos")
+ */
+
+
+//parte2
+
+
+const perguntaA = "Você trabalhou hoje?"
+const respostaA = prompt(perguntaA);
+const perguntaB = "Você comeu hoje?"
+const respostaB = prompt(perguntaB);
+const perguntaC = "Você bebeu agua hoje?"
+const respostaC = prompt(perguntaC);
+
+
+console.log("Você trabalhou hoje?  - " ,respostaA, )
+console.log("Você comeu hoje?  - " ,respostaB, )
+console.log("Você bebeu agua hoje?  - " ,respostaC, )

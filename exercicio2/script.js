b1e1812b1c58abf490b29c1cb4f6e4ac066cbf7d
victor felipe <vitinhose46@gmail.com>
2022-10-18 22:48:05 -0300

    exercicio 2 feito

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let a = 20
+let b = 40
+let c
+console.log("O valor de a é", a) // O novo valor de a é 20
+console.log("O valor de b é", b) // O novo valor de b é 40
+
+c = a
+a = b
+b = c 
+
+console.log("O novo valor de a é", a) // O novo valor de a é 40
+console.log("O novo valor de b é", b) // O novo valor de b é 20
